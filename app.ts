03e783271b6cb5ad03aa46321d1df32757187ebb
Drake Scott <dascott0204@gmail.com>
2017-03-09 23:58:07 -0600

    Block scope

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@
  */
 
 //let & const
+    console.log("Let and Const");
 let variable = "test";
 console.log(variable);
 variable = "Another Value";
@@ -11,3 +12,12 @@ console.log(variable);
 const maxLevels = 100;
 console.log(maxLevels);
 // maxLevels = 99; won't work
+
+// Block scope
+function reset() {
+    // console.log(variable);
+    let variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable);
