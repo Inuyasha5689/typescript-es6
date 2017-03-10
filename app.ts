8da06eeb980d385b06715020a9a2066c87746521
Drake Scott <dascott0204@gmail.com>
2017-03-10 00:02:30 -0600

    arrow functions

--- app.ts
+++ app.ts
@@ -21,3 +21,16 @@ function reset() {
 }
 reset();
 console.log(variable);
+
+// Arrow Functions
+
+console.log("Arrow Functions");
+const addNumbers = function (number1: number, number2: number): number {
+    return number1 + number2;
+};
+
+console.log(addNumbers(10, 3));
+
+const multiplyNumbers =  (number1: number, number2: number) => number1 * number2 ;
+
+console.log(multiplyNumbers(10, 3));
