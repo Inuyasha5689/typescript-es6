92ed14eb3734171081c7ee3e2a524644a62f7a49
Drake Scott <dascott0204@gmail.com>
2017-03-09 23:54:31 -0600

    let and const

---
+++ app.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by dasco on 3/9/2017.
+ */
+
+//let & const
+let variable = "test";
+console.log(variable);
+variable = "Another Value";
+console.log(variable);
+
+const maxLevels = 100;
+console.log(maxLevels);
+// maxLevels = 99; won't work
